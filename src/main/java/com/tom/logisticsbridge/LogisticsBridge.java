180f7301c7613afb98c27021e9199955779246d8
Domaman202 <vip.domaman@mail.ru>
2021-06-10 15:57:13 +0700

    Update LogisticsBridge.java

--- src/main/java/com/tom/logisticsbridge/LogisticsBridge.java
+++ src/main/java/com/tom/logisticsbridge/LogisticsBridge.java
@@ -34,7 +34,6 @@ import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.init.Items;
 import net.minecraft.inventory.IInventory;
 import net.minecraft.item.Item;
-import net.minecraft.item.ItemBlock;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.crafting.Ingredient;
 import net.minecraft.nbt.NBTTagCompound;
@@ -75,7 +74,7 @@ import java.util.stream.Stream;
 public class LogisticsBridge {
     public static final String ID = "logisticsbridge";
     public static final String NAME = "Logistics Bridge";
-    public static final String VERSION = "1.3.14";
+    public static final String VERSION = "1.4.1";
     public static final String DEPS = "after:appliedenergistics2;after:refinedstorage@[1.6.15,);required-after:logisticspipes@[0.10.3.39,)";
     public static final String UPDATE = "https://github.com/tom5454/LogisticsBridge/blob/master/version-check.json";
     public static final Logger log = LogManager.getLogger(NAME);
