d95ffc8ef806cf4c5f2e41bd1de31d64c28e8e5a
Li <nhatlinh.l195@gmail.com>
2021-07-06 15:30:03 +0700

    Update NetworkNodeSatellite.java

--- src/main/java/com/tom/logisticsbridge/node/NetworkNodeSatellite.java
+++ src/main/java/com/tom/logisticsbridge/node/NetworkNodeSatellite.java
@@ -22,7 +22,7 @@ import java.util.ListIterator;
 
 public class NetworkNodeSatellite extends NetworkNode implements IIdPipe {
     public static final String ID = "lb.satellite";
-    public String satelliteId;
+    public String satelliteId = "";
     private final List<ItemStack> itemsToInsert = new ArrayList<>();
 
     public NetworkNodeSatellite(World world, BlockPos pos) {
