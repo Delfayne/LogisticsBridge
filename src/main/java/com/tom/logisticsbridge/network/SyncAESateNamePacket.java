40b45e65108dfcd2e808d499e4768d2c0546c121
Li <nhatlinh.l195@gmail.com>
2021-07-15 10:48:51 +0700

    Update and rename SyncSatelliteBusPacket.java to SyncAESateNamePacket.java

--- src/main/java/com/tom/logisticsbridge/network/SyncAESateNamePacket.java
+++ src/main/java/com/tom/logisticsbridge/network/SyncAESateNamePacket.java
@@ -8,7 +8,7 @@ import net.minecraft.entity.player.EntityPlayer;
 import network.rs485.logisticspipes.SatellitePipe;
 
 @StaticResolve
-public class SyncSatelliteBusPacket extends SyncSatelliteNamePacket {
+public class SyncAESateNamePacket extends SyncSatelliteNamePacket {
 
     public SyncAESateNamePacket(int id) {
         super(id);
@@ -20,12 +20,12 @@ public class SyncSatelliteBusPacket extends SyncSatelliteNamePacket {
         if (pipe == null || pipe.pipe == null)
             return;
 
-        if (pipe.pipe instanceof SatelliteBus)
-            ((SatellitePipe) pipe.pipe).setSatelliteBusName(getString());
+        if (pipe.pipe instanceof SatellitePipe)
+            ((SatellitePipe) pipe.pipe).setSatellitePipeName(getString());
     }
 
     @Override
     public ModernPacket template() {
-        return new SyncSatelliteBusPacket(getId());
+        return new SyncAESateNamePacket(getId());
     }
 }
