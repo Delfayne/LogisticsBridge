9869c9b42e8c38bc70025d16892836df5497df0d
Domaman202 <vip.domaman@mail.ru>
2021-07-20 15:57:36 +0700

    Update LogisticsBridge.java

--- src/main/java/com/tom/logisticsbridge/LogisticsBridge.java
+++ src/main/java/com/tom/logisticsbridge/LogisticsBridge.java
@@ -76,7 +76,7 @@ public class LogisticsBridge {
     public static final String NAME = "Logistics Bridge";
     public static final String VERSION = "1.4.5";
     public static final String DEPS = "after:appliedenergistics2;after:refinedstorage@[1.6.15,);required-after:logisticspipes@[0.10.3.39,)";
-    public static final String UPDATE = "https://github.com/tom5454/LogisticsBridge/blob/master/version-check.json";
+    public static final String UPDATE = "https://github.com/Domaman202/LogisticsBridge/blob/master/version-check.json";
     public static final Logger log = LogManager.getLogger(NAME);
     private static final String CLIENT_PROXY_CLASS = "com.tom.logisticsbridge.proxy.ClientProxy";
     private static final String SERVER_PROXY_CLASS = "com.tom.logisticsbridge.proxy.ServerProxy";
